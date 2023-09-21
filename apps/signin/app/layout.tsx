5be0f5dbcba5d54bb8112af80a32b3864d5f875d
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-09-21 09:43:01 +0700

    add layout to signin page

--- apps/signin/app/layout.tsx
+++ apps/signin/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Layout } from '@nerdflix/shared/components';
 import './global.css';
 
 export const metadata = {
@@ -12,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
