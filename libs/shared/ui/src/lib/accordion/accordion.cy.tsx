5b43624a5f922a0d1255222bf66130d945c9bf4f
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-09-27 14:47:47 +0700

    fix: accordion component test

--- libs/shared/ui/src/lib/accordion/accordion.cy.tsx
+++ libs/shared/ui/src/lib/accordion/accordion.cy.tsx
@@ -22,17 +22,12 @@ describe(Accordion.name, () => {
 
     cy.get('@qOne').click();
     cy.contains(
-      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!",
-      { matchCase: false }
-    )
-      .parent()
-      .should('have.class', 'open');
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!"
+    ).should('be.visible');
+
     cy.get('@qOne').click();
     cy.contains(
-      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!",
-      { matchCase: false }
-    )
-      .parent()
-      .should('not.have.class', 'open');
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!"
+    ).should('not.be.visible');
   });
 });
